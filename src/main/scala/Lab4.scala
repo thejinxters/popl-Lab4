2c46ee1a0b4f955aa367e7b8fe04e5a4c2de5581
thejinxters <brandon.mikulka@colorado.edu>
2014-11-08 12:07:02 -0700

    finish type interpreter

--- src/main/scala/Lab4.scala
+++ src/main/scala/Lab4.scala
@@ -1,11 +1,11 @@
 object Lab4 extends jsy.util.JsyApplication {
   import jsy.lab4.ast._
   import jsy.lab4.Parser
-  
+
   /*
    * CSCI 3155: Lab 4
    * Brandon Mikulka
-   * 
+   *
    * Partner: Aaron Holt
    * Collaborators: <Any Collaborators>
    */
@@ -13,41 +13,41 @@ object Lab4 extends jsy.util.JsyApplication {
   /*
    * Fill in the appropriate portions above by replacing things delimited
    * by '<'... '>'.
-   * 
+   *
    * Replace 'YourIdentiKey' in the object name above with your IdentiKey.
-   * 
+   *
    * Replace the 'throw new UnsupportedOperationException' expression with
    * your code in each function.
-   * 
+   *
    * Do not make other modifications to this template, such as
    * - adding "extends App" or "extends Application" to your Lab object,
    * - adding a "main" method, and
    * - leaving any failing asserts.
-   * 
+   *
    * Your lab will not be graded if it does not compile.
-   * 
+   *
    * This template compiles without error. Before you submit comment out any
    * code that does not compile or causes a failing assert.  Simply put in a
    * 'throws new UnsupportedOperationException' as needed to get something
    * that compiles without error.
    */
-  
+
   /* Collections and Higher-Order Functions */
-  
+
   /* Lists */
-  
+
   def compressRec[A](l: List[A]): List[A] = l match {
     case Nil | _ :: Nil => l
     case h1 :: (t1 @ (h2 :: _)) => if (h1==h2) compressRec(t1) else h1::compressRec(t1)
   }
-  
+
   def compressFold[A](l: List[A]): List[A] = l.foldRight(Nil: List[A]){
     (h, acc) => acc match {
       case Nil => h::acc
       case h1 :: t1 => if (h==h1) h::t1 else h::acc
     }
   }
-  
+
   def mapFirst[A](f: A => Option[A])(l: List[A]): List[A] = l match {
     case Nil => l
     case h :: t => f(h) match{
@@ -55,15 +55,15 @@ object Lab4 extends jsy.util.JsyApplication {
       case None => h::mapFirst(f)(t)
     }
   }
-  
+
   /* Search Trees */
-  
+
   sealed abstract class Tree {
     def insert(n: Int): Tree = this match {
       case Empty => Node(Empty, n, Empty)
       case Node(l, d, r) => if (n < d) Node(l insert n, d, r) else Node(l, d, r insert n)
-    } 
-    
+    }
+
     def foldLeft[A](z: A)(f: (A, Int) => A): A = {
       def loop(acc: A, t: Tree): A = t match {
         case Empty => acc
@@ -75,35 +75,35 @@ object Lab4 extends jsy.util.JsyApplication {
       }
       loop(z, this)
     }
-    
+
     def pretty: String = {
       def p(acc: String, t: Tree, indent: Int): String = t match {
         case Empty => acc
         case Node(l, d, r) =>
           val spacer = " " * indent
           p("%s%d%n".format(spacer, d) + p(acc, l, indent + 2), r, indent + 2)
-      } 
+      }
       p("", this, 0)
     }
   }
   case object Empty extends Tree
   case class Node(l: Tree, d: Int, r: Tree) extends Tree
-  
+
   def treeFromList(l: List[Int]): Tree =
     l.foldLeft(Empty: Tree){ (acc, i) => acc insert i }
-  
+
   def sum(t: Tree): Int = t.foldLeft(0){ (acc, d) => acc + d }
-  
+
   def strictlyOrdered(t: Tree): Boolean = {
     val (b, _) = t.foldLeft((true, None: Option[Int])){
       (acc,d) => if (acc._2.getOrElse(0) < d) (acc._1 && true, Option(d)) else (acc._1 && false, None)
     }
     b
   }
-  
+
 
   /* Type Inference */
-  
+
   // A helper function to check whether a jsy type has a function type in it.
   // While this is completely given, this function is worth studying to see
   // how library functions are used.
@@ -112,7 +112,7 @@ object Lab4 extends jsy.util.JsyApplication {
     case TObj(fields) if (fields exists { case (_, t) => hasFunctionTyp(t) }) => true
     case _ => false
   }
-  
+
   def typeInfer(env: Map[String,Typ], e: Expr): Typ = {
     // Some shortcuts for convenience
     def typ(e1: Expr) = typeInfer(env, e1)
@@ -130,22 +130,38 @@ object Lab4 extends jsy.util.JsyApplication {
         case TNumber => TNumber
         case tgot => err(tgot, e1)
       }
-      case Unary(Not, e1) =>
-        throw new UnsupportedOperationException
-      case Binary(Plus, e1, e2) =>
-        throw new UnsupportedOperationException
-      case Binary(Minus|Times|Div, e1, e2) => 
-        throw new UnsupportedOperationException
-      case Binary(Eq|Ne, e1, e2) =>
-        throw new UnsupportedOperationException
-      case Binary(Lt|Le|Gt|Ge, e1, e2) =>
-        throw new UnsupportedOperationException
-      case Binary(And|Or, e1, e2) =>
-        throw new UnsupportedOperationException
-      case Binary(Seq, e1, e2) =>
-        throw new UnsupportedOperationException
-      case If(e1, e2, e3) =>
-        throw new UnsupportedOperationException
+      case Unary(Not, e1) => typ(e1) match{
+        case TBool => TBool
+        case tgot => err(tgot,e1)
+      }
+      case Binary(Plus, e1, e2) => (typ(e1), typ(e2)) match {
+        case (TString, TString) => TString
+        case (TNumber, TNumber) => TNumber
+        case _ => err(typ(e1), e1)
+      }
+      case Binary(Minus|Times|Div, e1, e2) => (typ(e1), typ(e2)) match {
+        case (TNumber, TNumber) => TNumber
+        case _ => err(typ(e1), e1)
+      }
+      case Binary(Eq|Ne, e1, e2) => (typ(e1), typ(e2)) match{
+        case (TFunction(params, tret), _) => err(typ(e1), e1)
+        case (_, TFunction(params, tret)) => err(typ(e2), e2)
+        case (t1, t2) => if (t1==t2) TBool else err(typ(e2), e2)
+      }
+      case Binary(Lt|Le|Gt|Ge, e1, e2) => (typ(e1), typ(e2)) match {
+        case (TString, TString) => TBool
+        case (TNumber, TNumber) => TBool
+        case _ => err(typ(e1), e1)
+      }
+      case Binary(And|Or, e1, e2) => (typ(e1), typ(e2)) match {
+        case (TBool, TBool) => TBool
+        case _ => err(typ(e1), e1)
+      }
+      case Binary(Seq, e1, e2) => typ(e2)
+      case If(e1, e2, e3) => typ(e1) match{
+        case TBool => if (typ(e2) == typ(e3)) typ(e2) else err(typ(e2), e2)
+        case _ => err(typ(e1), e1)
+      }
       case Function(p, params, tann, e1) => {
         // Bind to env1 an environment that extends env with an appropriate binding if
         // the function is potentially recursive.
@@ -157,32 +173,48 @@ object Lab4 extends jsy.util.JsyApplication {
           case _ => err(TUndefined, e1)
         }
         // Bind to env2 an environment that extends env1 with bindings for params.
-        val env2 = throw new UnsupportedOperationException
+        val env2 =  params.foldLeft(env1)((acc, x) => x match {
+          case (s,t) => acc + (s -> t)
+        })
         // Match on whether the return type is specified.
         tann match {
-          case None => throw new UnsupportedOperationException
-          case Some(tret) => throw new UnsupportedOperationException
+          case None => {
+            val t = typeInfer(env2, e1)
+            TFunction(params, t)
+          }
+          case Some(tret) => {
+            if (typeInfer(env2, e1) == tret) TFunction(params, tret)
+            else err(typeInfer(env2, e1), e1)
+          }
         }
+
       }
       case Call(e1, args) => typ(e1) match {
-        case TFunction(params, tret) if (params.length == args.length) => {
+        case TFunction(params, tret) if params.length == args.length => {
           (params, args).zipped.foreach {
-            throw new UnsupportedOperationException
-          };
+            (p, e1) => if(p._2 != typ(e1)) return err(typ(e1), e1)
+          }
           tret
         }
         case tgot => err(tgot, e1)
       }
-      case Obj(fields) =>
-        throw new UnsupportedOperationException
-      case GetField(e1, f) =>
-        throw new UnsupportedOperationException
+      case Obj(fields) => {
+        //TODO: Rewrite with a case match
+        TObj( fields.map{ case (x,y) => (x,typ(y))} )
+      }
+      case GetField(e1, f) => typ(e1) match{
+        case TObj(tfields) => tfields.get(f) match{
+          case Some(t1) => t1
+          case None => err(typ(e1), e1)
+        }
+        case _ => err(typ(e1), e1)
+      }
     }
   }
-  
-  
+
+
   /* Small-Step Interpreter */
-  
+
   def inequalityVal(bop: Bop, v1: Expr, v2: Expr): Boolean = {
     require(bop == Lt || bop == Le || bop == Gt || bop == Ge)
     ((v1, v2): @unchecked) match {
@@ -202,12 +234,12 @@ object Lab4 extends jsy.util.JsyApplication {
         }
     }
   }
-  
+
   def substitute(e: Expr, v: Expr, x: String): Expr = {
     require(isValue(v))
-    
+
     def subst(e: Expr): Expr = substitute(e, v, x)
-    
+
     e match {
       case N(_) | B(_) | Undefined | S(_) => e
       case Print(e1) => Print(subst(e1))
@@ -226,12 +258,12 @@ object Lab4 extends jsy.util.JsyApplication {
         throw new UnsupportedOperationException
     }
   }
-  
+
   def step(e: Expr): Expr = {
     require(!isValue(e))
-    
+
     def stepIfNotValue(e: Expr): Option[Expr] = if (isValue(e)) None else Some(step(e))
-    
+
     e match {
       /* Base Cases: Do Rules */
       case Print(v1) if isValue(v1) => println(pretty(v1)); Undefined
@@ -240,11 +272,15 @@ object Lab4 extends jsy.util.JsyApplication {
       case Binary(Seq, v1, e2) if isValue(v1) => e2
       case Binary(Plus, S(s1), S(s2)) => S(s1 + s2)
       case Binary(Plus, N(n1), N(n2)) => N(n1 + n2)
+      case Binary(Minus, N(n1), N(n2)) => N(n1 - n2)
+      case Binary(Times, N(n1), N(n2)) => N(n1 * n2)
+      case Binary(Div, N(n1), N(n2)) => N(n1 / n2)
       case Binary(bop @ (Lt|Le|Gt|Ge), v1, v2) if isValue(v1) && isValue(v2) => B(inequalityVal(bop, v1, v2))
       case Binary(Eq, v1, v2) if isValue(v1) && isValue(v2) => B(v1 == v2)
       case Binary(Ne, v1, v2) if isValue(v1) && isValue(v2) => B(v1 != v2)
       case Binary(And, B(b1), e2) => if (b1) e2 else B(false)
       case Binary(Or, B(b1), e2) => if (b1) B(true) else e2
+      case If(B(b1), e2, e3)  => if (b1) e2 else e3
       case ConstDecl(x, v1, e2) if isValue(v1) => substitute(e2, v1, x)
       case Call(v1, args) if isValue(v1) && (args forall isValue) =>
         v1 match {
@@ -260,7 +296,7 @@ object Lab4 extends jsy.util.JsyApplication {
           case _ => throw new StuckError(e)
         }
       /*** Fill-in more cases here. ***/
-        
+
       /* Inductive Cases: Search Rules */
       case Print(e1) => Print(step(e1))
       case Unary(uop, e1) => Unary(uop, step(e1))
@@ -269,30 +305,30 @@ object Lab4 extends jsy.util.JsyApplication {
       case If(e1, e2, e3) => If(step(e1), e2, e3)
       case ConstDecl(x, e1, e2) => ConstDecl(x, step(e1), e2)
       /*** Fill-in more cases here. ***/
-      
+
       /* Everything else is a stuck error. Should not happen if e is well-typed. */
       case _ => throw StuckError(e)
     }
   }
-  
-  
+
+
   /* External Interfaces */
-  
+
   this.debug = true // comment this out or set to false if you don't want print debugging information
-  
+
   def inferType(e: Expr): Typ = {
     if (debug) {
       println("------------------------------------------------------------")
       println("Type checking: %s ...".format(e))
-    } 
+    }
     val t = typeInfer(Map.empty, e)
     if (debug) {
       println("Type: " + pretty(t))
     }
     t
   }
-  
-  // Interface to run your small-step interpreter and print out the steps of evaluation if debugging. 
+
+  // Interface to run your small-step interpreter and print out the steps of evaluation if debugging.
   def iterateStep(e: Expr): Expr = {
     require(closed(e))
     def loop(e: Expr, n: Int): Expr = {
@@ -312,7 +348,7 @@ object Lab4 extends jsy.util.JsyApplication {
 
   // Convenience to pass in a jsy expression as a string.
   def iterateStep(s: String): Expr = iterateStep(Parser.parse(s))
-  
+
   // Interface for main
   def processFile(file: java.io.File) {
     if (debug) {
@@ -320,18 +356,18 @@ object Lab4 extends jsy.util.JsyApplication {
       println("File: " + file.getName)
       println("Parsing ...")
     }
-    
+
     val expr =
       handle(None: Option[Expr]) {Some{
         Parser.parseFile(file)
       }} getOrElse {
         return
       }
-    
+
     handle() {
       val t = inferType(expr)
     }
-    
+
     handle() {
       val v1 = iterateStep(expr)
       println(pretty(v1))
